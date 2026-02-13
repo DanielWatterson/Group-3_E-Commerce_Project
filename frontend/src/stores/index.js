965d931f05dda99d969a4b3954856e9074ee52ab
Mogamat Toufeeq farat <toufeeqfarat@gmail.com>
2026-02-13 11:45:46 +0200

    initialize TheWelcome component and set up Vuex store with product management

--- frontend/src/stores/index.js
+++ frontend/src/stores/index.js
@@ -1,15 +1,24 @@
-import { createStore } from 'vuex';
+import { createStore } from "vuex";
 const store = createStore({
-    // Your Vuex store configuration
-    state:{
-
-    },
-    mutations:{
-
-    },
-    actions:{
-
-    }
+    state: {
+        products: [],
+    },
+    mutations: {
+        getProducts(state, payload) {
+            state.products = payload;
+        },
+    },
+    actions: {
+        async getProducts({ commit }) {
+            try {
+                const res = await axios.get(`http://localhost:5050/products`);
+                commit("getProducts", res.data);
+            } catch (error) {
+                console.error("Failed to fetch getProducts:", error);
+                throw error;
+            }
+        },
+    },
 });
 
-export default store
+export default store;
