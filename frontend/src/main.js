a9334ac99654cde11f981b32b71c89c91bf53dce
Mogamat Toufeeq farat <toufeeqfarat@gmail.com>
2026-02-20 09:33:22 +0200

    updated ui

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,13 +2,29 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import store from './stores'
-
 import App from './App.vue'
 import router from './router'
+
+import PrimeVue from 'primevue/config'
+import Aura from '@primevue/themes/aura'
+import 'primeicons/primeicons.css'
+
+import Menubar from 'primevue/menubar'
+import Button from 'primevue/button'
 
 const app = createApp(App)
 
 app.use(store)
 app.use(router)
+
+app.use(PrimeVue, {
+    ripple: true,
+    theme: {
+        preset: Aura
+    }
+})
+
+app.component('Menubar', Menubar)
+app.component('Button', Button)
 
 app.mount('#app')
